24215e2bcabfb5f6059f0e100da5f6568d3fa5bd
Abhijeet <abhijeetlovesmahi@gmail.com>
2026-02-16 23:33:46 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDqDmsMp2eAuHJBcjW-ciO2JcLTXapiIrs",
+  authDomain: "the-lalit-d7472.firebaseapp.com",
+  projectId: "the-lalit-d7472",
+  appId: "1:479237084229:web:31078825739b3c5712ff2c"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
